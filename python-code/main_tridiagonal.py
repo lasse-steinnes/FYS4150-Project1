ebc35fa36bb0d50767309e29ee96fc2dd445ee06
Lasse Steinnes <lasse.steinnes93@gmail.com>
2020-08-30 16:59:58 +0200

    Functional code for special case

---
+++ python-code/main_tridiagonal.py
@@ -0,0 +1,40 @@
+###  Runs the specific algorithm for a tridiagonal matrix with same
+###  non-diagonal elements
+
+from poisson_difference import initializer, forward_sbst, backward_sbtst
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+
+# get input
+"""
+Note: To use this code you need to add
+- a, b: solution interval for x param
+- func: source function
+- d_val: Diagonal element values
+- e_val: Non-diagonal value elements
+- n: points
+"""
+parser = argparse.ArgumentParser() # Creating parser
+parser.add_argument('a'); parser.add_argument('b');
+parser.add_argument('d_val'); parser.add_argument('e_val')
+parser.add_argument('n')
+args = parser.parse_args()
+
+# initalize matrix elements
+d,e,rhs,u_exact, x = initializer(float(args.a), float(args.b),float(args.d_val),\
+                      float(args.e_val),int(args.n))
+
+# Perform forward substitution
+d_new, new_rhs = forward_sbst(d,rhs,int(args.n))
+
+# Solve numerically with backward substitution
+u = np.zeros(int(args.n)); u[0] = 0; u[int(args.n)-1] = 0;
+U_num = backward_sbtst(u,d_new, new_rhs,int(args.n))
+
+
+# plot numerical and analytical against each other
+plt.plot(x,U_num,'-.',label = "numerical")
+plt.plot(x,u_exact,'--',label = "exact")
+plt.legend()
+plt.show()
--- python-code/poisson_difference.py
+++ python-code/poisson_difference.py
@@ -1,29 +1,52 @@
 ### Python code for project 1c: Specific solution for the 1D poission equation ###
-import numpy as np
+import numpy as np  # array operations
 
-def initializer(a,b, func,d_val,e_val, n):
-    h_step = (b-a)/(n+1);
-    d = np.full((1,n),d_val, dtype = float); # diagonal (2)
-    e = np.full((1,n),e_val, dtype = float); e[n] = 0;  # non-diagonal (-1)
-    rhs = func() # do something with this to make array: Use symbolic python
-    return
+def initializer(a,b,d_val,e_val, n):
+    """
+    Function that initializes arrays and source
+    Input:
+    - a, b: solution interval for x param
+    - func: source function
+    - d_val: Diagonal element values
+    - e_val: Non-diagonal value elements
+    - n: points
+    """
+    h_step = (b-a)/(n-1)
+    d = np.full((n,1),d_val, dtype = float); # diagonal (2)
+    e = np.full((n,1),e_val, dtype = float); e[n-1] = 0;  # non-diagonal (-1)
+    x = np.linspace(a,b,n)
+    rhs = (h_step**2)*100*np.exp(-10*x) # do something with this to make array: Use symbolic python
+    u_exact = 1-(1-np.exp(-10))*x-np.exp(-10*x)
+    return d, e, rhs, u_exact, x
 
-def forward_sbst(d,rhs):
+def forward_sbst(d,rhs,n):
     """
     Calculates forward substitution
     for tridiagonal matrix only
+    Input:
+    - d: diagonal elements (array)
+    - rhs: source (array)
     """
     for i in range(1,n):
         d[i] = (i + 1)/i
-        rhs[i] = rhs[i] + (i-1)/i*f[i-1]
+        rhs[i] = rhs[i] + (i-1)/i*rhs[i-1]
+    return d, rhs
 
-def backward_sbtst(u,rhs):
+def backward_sbtst(u,d,f,n):
     """
     Calculates backward substition
     for tridiagonal matrix only
+    Input:
+    - u: an array with known endpoints
+    - d: diagonal array changed by forward_sbst
+    - f: source function altered by forward_sbstn
+    - n: number of points in array
     """
-    for i in range(n-1,1,-1):
-        u[i-1] = (i-1)/i*(rhs[i-1] + u[i])
-    u[n] = f[n]/b[n]
+    for i in range(n-2,1,-1): # n points, index n-1 last
+        u[i-1] = (i-1)/i*(f[i-1] + u[i])
+    u[n-1] = f[n-1]/d[n-1]
 
     return u
+
+
+#    h_step = (b-a)/(n-1); # not necessary (could have been useful in for loop)
